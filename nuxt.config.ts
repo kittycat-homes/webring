e34810fe4cbad351bbecd5d0a2cdebaa32cfb017
bat <zoe@kittycat.homes>
2024-02-11 14:48:00 +0100

    everyone!

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
   nitro: {
     prerender: {
       crawlLinks: true,
-      routes: memberRoutes()
-    }
-  }
-})
+      routes: memberRoutes().concat(["/members/all.json"]),
+    },
+  },
+});
---
+++ server/routes/everyone.json.ts
@@ -0,0 +1,6 @@
+import { doesMemberAndPathMatch, memberInRelation, members } from "~/members";
+
+export default defineEventHandler(async (event) => {
+  event.node.res.setHeader("content-type", "text/json");
+  event.node.res.end(JSON.stringify(members));
+});
