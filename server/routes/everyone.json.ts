4adbde3e608647556de270b4ff27b039630e969a
bat <zoe@kittycat.homes>
2024-02-11 14:59:39 +0100

    correct content type in json files

--- server/routes/everyone.json.ts
+++ server/routes/everyone.json.ts
@@ -1,6 +1,6 @@
 import { doesMemberAndPathMatch, memberInRelation, members } from "~/members";
 
 export default defineEventHandler(async (event) => {
-  event.node.res.setHeader("content-type", "text/json");
+  event.node.res.setHeader("content-type", "application/json");
   event.node.res.end(JSON.stringify(members));
 });
--- server/routes/members/[name].json.ts
+++ server/routes/members/[name].json.ts
@@ -5,6 +5,6 @@ export default defineEventHandler(async (event) => {
     return doesMemberAndPathMatch(member, event.path);
   });
   const memberRelation = memberInRelation(memberIndex);
-  event.node.res.setHeader("content-type", "text/json");
+  event.node.res.setHeader("content-type", "application/json");
   event.node.res.end(JSON.stringify(memberRelation));
 });
