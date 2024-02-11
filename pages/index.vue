991f1fbdc43ea76e939462445a7f861271687f79
bat <zoe@kittycat.homes>
2024-02-11 19:05:45 +0100

    text to the left

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="grid min-h-screen gap-5 p-5 @container">
+  <div class="grid min-h-screen gap-5 p-5">
     <h1 class="font-display text-7xl text-primary">kittycat.homes webring™</h1>
-    <div class="flex flex-col gap-5 lg:flex-row">
-      <MemberList class="flex-grow-[14] flex-1" />
-      <div class="flex-grow-[3] grid w-full flex-1 gap-5 @7xl:grid-cols-3">
-        <AboutWebsite class="@7xl:col-span-2" />
-        <JoinWebring />
+    <div class="flex flex-col gap-5 lg:flex-row-reverse">
+      <MemberList class="flex-1 flex-grow-[14] p-5" />
+      <div class="flex-1 flex-grow-[3] @container p-5">
+        <div class="grid w-full gap-5 @4xl:grid-cols-3">
+          <AboutWebsite class="@4xl:col-span-2" />
+          <JoinWebring />
+        </div>
       </div>
     </div>
     <footer class="text-center text-sm">
