92018b1706a56054a66e051518085eebf1896619
bat <zoe@kittycat.homes>
2024-02-11 19:10:20 +0100

    awawawa

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="grid min-h-screen gap-5 p-5">
-    <h1 class="font-display text-7xl text-primary">kittycat.homes webring™</h1>
+    <h1 class="pb-32 pt-32 text-center font-display text-7xl text-primary">
+      kittycat.homes webring™
+    </h1>
     <div class="flex flex-col gap-5 lg:flex-row-reverse">
       <MemberList class="flex-1 flex-grow-[14] p-5" />
-      <div class="flex-1 flex-grow-[3] @container p-5">
+      <div class="flex-1 flex-grow-[3] p-5 @container">
         <div class="grid w-full gap-5 @4xl:grid-cols-3">
           <AboutWebsite class="@4xl:col-span-2" />
           <JoinWebring />
