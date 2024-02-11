2912d1d9cfd7a65690161ac21a0aebff4281defb
bat <zoe@kittycat.homes>
2024-02-11 20:53:14 +0100

    add viewport scale

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@ import { memberRoutes } from "./members";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+    },
+  },
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   postcss: {
