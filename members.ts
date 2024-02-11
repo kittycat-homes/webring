a6b136817794f5da877feafaae31f387e6d33b8b
bat <zoe@kittycat.homes>
2024-02-11 13:41:35 +0100

    initial commit

---
+++ members.ts
@@ -0,0 +1,23 @@
+export type MemberImage = {
+  description: string;
+  url: URL;
+};
+
+export type Member = {
+  name: string;
+  url: URL;
+  description?: string;
+  image?: MemberImage;
+};
+
+export const members: Array<Member> = [
+  {
+    name: "tiger",
+    url: new URL("https://tiger.kittycat.homes"),
+    description: "the tiger",
+  },
+  {
+    name: "zoe",
+    url: new URL("https://zoe.kittycat.homes"),
+  },
+];
