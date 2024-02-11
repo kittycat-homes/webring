b48c1fef0196d7e95b99c67701ab4c434befbcc2
bat <zoe@kittycat.homes>
2024-02-11 14:17:59 +0100

    display the json

--- members.ts
+++ members.ts
@@ -4,26 +4,54 @@ export type MemberImage = {
 };
 
 export type Member = {
-  name: string;
+  displayname: string;
+  username: string;
   url: URL;
   description?: string;
   image?: MemberImage;
 };
 
+// add new members here!
 export const members: Array<Member> = [
   {
-    name: "tiger",
+    displayname: "tiger",
+    username: "tiger",
     url: new URL("https://tiger.kittycat.homes"),
     description: "the tiger",
   },
   {
-    name: "zoe",
+    displayname: "zoe",
+    username: "zoe",
     url: new URL("https://zoe.kittycat.homes"),
+  },
+  {
+    displayname: "real mc personface",
+    username: "notfake",
+    url: new URL("https://example.com"),
   },
 ];
 
+// tells the router where the json pages for members are
 export function memberRoutes(): Array<string> {
   return members.map((member) => {
-    return `members/${member.name}.json`;
+    return `/members/${member.username}.json`;
   });
+}
+
+export function doesMemberAndPathMatch(member: Member, path: string): boolean {
+  return path === `/members/${member.username}.json`;
+}
+
+export type MemberRelation = {
+  last: Member;
+  current: Member;
+  next: Member;
+};
+
+export function memberInRelation(index: number): MemberRelation {
+  return {
+    last: members[(index - 1) % members.length],
+    current: members[index % members.length],
+    next: members[(index + 1) % members.length],
+  };
 }
--- server/routes/members/[name].json.ts
+++ server/routes/members/[name].json.ts
@@ -1,6 +1,10 @@
-import { members } from "~/members";
+import { doesMemberAndPathMatch, memberInRelation, members } from "~/members";
 
 export default defineEventHandler(async (event) => {
+  const memberIndex = members.findIndex((member) => {
+    return doesMemberAndPathMatch(member, event.path);
+  });
+  const memberRelation = memberInRelation(memberIndex);
   event.node.res.setHeader("content-type", "text/json");
-  event.node.res.end(JSON.stringify(members));
+  event.node.res.end(JSON.stringify(memberRelation));
 });
