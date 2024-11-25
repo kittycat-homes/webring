6a94454fc4e89b03686f85305dfa43cdaa6a2653
bat <zoe@kittycat.homes>
2024-11-25 22:41:04 +0100

    update

--- components/AboutWebsite.vue
+++ components/AboutWebsite.vue
@@ -2,11 +2,7 @@
   <div>
     <h2 class="font-display text-3xl text-primary">about</h2>
     <p class="prose">
-      this is THE webring...... of kittycat.homes. what's kittycat.homes you
-      ask? it's a website... zoe and tiger r on it and they r literally
-      girlfriends. so they have a website domain TOGeTHER. then they were like
-      wow theres gotta be a webring out there. so they did it. theyre mad. in
-      the head. anyone can come into teh webring NOW check out the join section. to join..
+      ✌️
     </p>
   </div>
 </template>
